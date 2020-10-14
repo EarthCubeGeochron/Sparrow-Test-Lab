b55af5bf28472ad7da778b52be2ee1163f97e577
Daven Quinn <dev@davenquinn.com>
2020-10-14 10:06:56 -0500

    Updated overrides

--- Makefile
+++ Makefile
@@ -1,4 +1,5 @@
 all:
 	git submodule update --init --recursive
+	cp pgweb-addon/nginx-location.conf Sparrow/nginx-config/locations/pgweb.conf
 	cd Sparrow && SPARROW_INSTALL_PATH=.. make install-dev
 	bin/sparrow up
--- sparrow-config.overrides.sh
+++ sparrow-config.overrides.sh
@@ -1,3 +1,5 @@
 # Secrets
 export MAPBOX_API_TOKEN="pk.eyJ1IjoiamN6YXBsZXdza2kiLCJhIjoiY2szNXA5OWcxMDN2bzNtcnI1cWd1ZXJpYiJ9.Dd5GKlrPhg969y1ayY32cg"
 export SPARROW_SECRET_KEY="Test123"
+export SPARROW_COMPOSE_OVERRIDES="${SPARROW_COMPOSE_OVERRIDES}:/projects/sparrow-data.org/test-lab-override.yaml"
+export SPARROW_BASE_URL="/labs/basic-test/"
