ffa79b002d53da6424059361751b2a1a0050bf96
Daven Quinn <dev@davenquinn.com>
2020-10-14 03:35:34 -0500

    Successfully load test data

--- plugins/__init__.py
+++ plugins/__init__.py
@@ -3,8 +3,14 @@ from sparrow.database import User, on_conflict
 from json import load
 from os import path
 
+_test_data = path.abspath(
+    path.join(path.dirname(__file__), "test-data", "project-1.json")
+)
+
 
 class TestInitPlugin(SparrowPlugin):
+    """A plugin to initialize a test dataset"""
+
     name = "test-init"
 
     def on_database_ready(self, db):
@@ -16,10 +22,13 @@ class TestInitPlugin(SparrowPlugin):
             db.session.commit()
 
     def on_load_complete(self):
+        ## Todo: running importers on app lifecycle methods is a bad idea,
+        # because we might run the method on several workers in production.
         db = self.app.database
-        fn = path.abspath(
-            path.join(path.dirname(__file__), "test-data", "project-1.json")
-        )
-        with open(fn, "r") as f:
-            json_data = load(f)
-        db.load_data("project", json_data["data"])
+        project_count = db.session.query(db.model.project).count()
+        if project_count > 0:
+            return
+        db = self.app.database
+        with open(_test_data, "r") as f:
+            data = load(f)
+        db.load_data("project", data["data"])
