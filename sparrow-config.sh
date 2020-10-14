093bfacee11237cda310158301f3e2f5281c3698
Daven Quinn <dev@davenquinn.com>
2020-10-14 04:42:26 -0500

    Add pgweb

--- sparrow-config.sh
+++ sparrow-config.sh
@@ -1,6 +1,7 @@
 export SPARROW_LAB_NAME="Test Lab"
 export SPARROW_ENV="development"
 export SPARROW_PLUGIN_DIR="$SPARROW_CONFIG_DIR/plugins"
+export SPARROW_COMPOSE_OVERRIDES="$SPARROW_CONFIG_DIR/pgweb-addon/docker-compose.override.yaml"
 
 # Get "secret" config values from an overrides file.
 source "$SPARROW_CONFIG_DIR/sparrow-config.overrides.sh"
